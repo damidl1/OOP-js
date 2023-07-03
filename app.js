948d4378ad77ab88bd81b6eb19e25c26e99023eb
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-07-03 10:55:36 +0200

    book

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+const book1 = {
+    title: 'iliade',
+    author: 'omero',
+    dop: '27/12/2009',    //date of publication
+    publisher: 'einaudi',
+}
+
+
+
+const book2 = new Book('odissea', 'omero', '27/12/2010', 'einaudi');
+
+console.log(book1);
+console.log(book2);
+
+
+console.log(book1.title);
+console.log(book2.title);
+
+
+console.log(book1['author']);
+console.log(book2['author']);
+
+printBookData()
---
+++ book.js
@@ -0,0 +1,14 @@
+
+
+class Book{    // abbiamo creato lo stampino per poi generare oggetti di tipo libro
+    
+  constructor(title, author, dop, publisher){       // dice di cosa ha bisogno il libro per essere costruito
+
+      this.title = title;       // esempio la caratteristica title dell'oggetto è uguale al parametro title che ti verrà passato e così via
+      this.author = author; 
+      this.dop = dop;
+      this.publisher = publisher;
+   
+   } 
+
+}
